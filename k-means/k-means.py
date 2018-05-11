53155a67532b20762948176af10437c5b5b52794
Jamie Huang <jamie.huang@gmail.com>
2018-05-11 16:23:03 -0700

    Add files via upload

--- k-means/k-means.py
+++ k-means/k-means.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -12,85 +11,30 @@ def __init__(self, data, i3, wvl, create_spectral_map,
     '''
     initializes variables
     '''
-    ### JMS I modified this description and the indent of this def __init__.
-    ### JMS Note also that I breack the first line. This is to follow pep8
-    
 
     self.create_spectral_map = create_spectral_map
     self.create_data_cube = create_data_cube
     self.k-means_maps = k-means_maps
+    self.i3 = i3
 
-def read_data(self, i3, wvl, npzfilename = ""/net/opal/Volumes/Amnesia/mpi3drun/2Druns/genohm/rain/new_inte1_02.npy.npz"") ### JMS typo, Extra" in each side of the file  
-'''reads data, loads data, saves the loading of the data'''
+def read_data(self, i3, wvl, kmeansfilename = "k-means.pck", rbfilename = "rb", npzfilename = "/net/opal/Volumes/Amnesia/mpi3drun/2Druns/genohm/rain/new_inte1_02.npy.npz"):
+	'''
+	reads data, loads data, saves the loading of the data
+	'''
 
 	self.data = np.load(npzfilename)
 	self.i3 = data["arr_0"]
 	self.wvl = data["arr_1"]
-	self.pick_in = open('k-means.pck', 'rb') ### JMS this is hard coded, put an input similar to npzfilename
+	self.pick_in = open(kmeansfilename, rbfilename) 
 	self.km = pickle.load(pick_in)
-		
-def create_spectral_map(self, i3, wvl, ax):  ###JMS this is a sanity plot, put at the end
-'''reshapes into 2D array
-   interpolation
-   adjusts wvl axis
-   creates a spectral profile map showing the location of the clusters in all the labels as a whole
-   inputs: i3, wvl, ax, outputs: prints the image of the spectral map'''
 
-	i3_2D = i3.reshape((i3.shape[0]*i3.shape[1], i3.shape[2])) ###JMS Note i3 and  wvl are defined in read_data. So no need to put input and use self.i3 and self.wvl
-	wvl_new = wvl*10.-2795.37
-	plt.subplots_adjust(bottom=0.2, top=.9, left=0.15)
-	axes_style = {'linewidth':2}
-	plt.imshow(i3[0,:,:], aspect = 'auto', extent=(np.min(wvl_new),np.max(wvl_new), 1570, 0))
-	plt.title('Spectral Profile Map for Mg II k & h', fontsize = 15)
-	plt.xlabel('Wavelength - 2796.2 [$\AA$]', fontsize = 15)
-	axes_style = {'linewidth':2}
-	plt.ylabel('Time (s)', fontsize = 15)
-	ax = plt.gca()
-	ax.invert_yaxis()
-	plt.savefig('spectral_map.eps')
-	plt.show()
+def time_import(self, tm, inertia, t0):
 
-def create_km_map(self) ###JMS this is a sanity plot, put at the end
-'''creates the image of the km_map_datacube  
-   shows the locations of the k-means clusters for each labels
-   outputs: prints the image of the km_map datacube'''
-
-	plt.figure(figsize = (5,5))
-	axes_style = {'linewidth':2}
-	plt.subplots_adjust(bottom=0.15, top=.9, left=0.15)
-	km_map_all = np.sum(km_map_datacube[:,60:,:], axis=2)
-	plt.imshow(km_map_all, extent=[0,157,0,3000], aspect='auto', cmap='rainbow')
-	plt.xlabel('Time [Seconds]')
-	plt.ylabel('Y [Pixels]')
-	plt.colorbar()
-	plt.show()
-
-def create_k_means_maps(self, wvl)  ###JMS this is a sanity plot, put at the end
-'''creates the k_means maps 
-   plots the spectral profile for the different k-means labels
-   wavelength on the x axis and intensity on the y axis
-   inputs: wvl, outputs: prints the image of the k-means labels'''
-
-	plt.figure(figsize = (30,30))
-	for i in range(0, 30):
-        plt.subplot(5,6,i+1)
-        plt.xlabel('Wavelength - 2796.2 [$\AA$]', fontsize=20)
-        plt.ylabel('Intensity/I_{c} [$erg s^{-1} cm^{-2} Hz^{-1} ster^{-1}$]', fontsize=20)
-        plt.plot(wvl*10.-2795.37, km.cluster_centers_[i,0:2000]*1e3/3.454e-6)
-	plt.show()
-
-def fit(self, t0) ###JMS this should be prior the maps. 
-'''uses the MiniBatchKMeans function to fit the i3_2D data into clusters
-   inputs: t0'''
-
-	t0 = time.time()
-	mini_km = MiniBatchKMeans(n_clusters=30).fit(self.i3[:,1000:2000]) # JMS self.i3 must be defined! (see line 
-	t_mini_batch = time.time() - t0
-
-def time_import(tm, inertia, t0)  ###JMS Missing self. This should be prior the maps. 
-'''uses the MiniBatchKMeans function to fit the i3_2D data into clusters
-   computes the inertia of the MiniBatchKMeans
-   inputs: tm, inertia, t0, outputs: '''
+	'''
+	uses the MiniBatchKMeans function to fit the i3_2D data into clusters
+   	computes the inertia of the MiniBatchKMeans
+    inputs: tm, inertia, t0, outputs: 
+    '''
 
 	tm = np.zeros(30)
 	inertia = np.zeros(30)
@@ -108,5 +52,70 @@ def time_import(tm, inertia, t0)  ###JMS Missing self. This should be prior the 
 	plt.plot(inertia)
 	plt.show()
 
-$ pip install pep8  ###JMS Remove these two lines. 
-$ pip install autopep8
+def fit(self, t0):
+	'''
+	uses the MiniBatchKMeans function to fit the i3_2D data into clusters
+	inputs: t0
+	'''
+
+	t0 = time.time()
+	mini_km = MiniBatchKMeans(n_clusters=30).fit(self.i3[:,1000:2000])
+	t_mini_batch = time.time() - t0
+
+
+def create_km_map(self):
+
+	'''
+	creates the image of the km_map_datacube  
+   	shows the locations of the k-means clusters for each labels
+   	outputs: prints the image of the km_map datacube
+   	'''
+
+	plt.figure(figsize = (5,5))
+	axes_style = {'linewidth':2}
+	plt.subplots_adjust(bottom=0.15, top=.9, left=0.15)
+	km_map_all = np.sum(km_map_datacube[:,60:,:], axis=2)
+	plt.imshow(km_map_all, extent=[0,157,0,3000], aspect='auto', cmap='rainbow')
+	plt.xlabel('Time [Seconds]')
+	plt.ylabel('Y [Pixels]')
+	plt.colorbar()
+	plt.show()
+
+def create_k_means_maps(self, wvl):
+	'''
+	creates the k_means maps 
+	plots the spectral profile for the different k-means labels
+	wavelength on the x axis and intensity on the y axis
+	inputs: wvl, outputs: prints the image of the k-means labels
+	'''
+
+	plt.figure(figsize = (30,30))
+	for i in range(0, 30):
+        plt.subplot(5,6,i+1)
+        plt.xlabel('Wavelength - 2796.2 [$\AA$]', fontsize=20)
+        plt.ylabel('Intensity/I_{c} [$erg s^{-1} cm^{-2} Hz^{-1} ster^{-1}$]', fontsize=20)
+        plt.plot(wvl*10.-2795.37, km.cluster_centers_[i,0:2000]*1e3/3.454e-6)
+
+ def create_spectral_map(self, i3, wvl, ax):
+	
+	'''
+	reshapes into 2D array
+   	interpolation
+   	adjusts wvl axis
+   	creates a spectral profile map showing the location of the clusters in all the labels as a whole
+   	inputs: i3, wvl, ax, outputs: prints the image of the spectral map
+   	'''
+
+	wvl_new = wvl*10.-2795.37
+	plt.subplots_adjust(bottom=0.2, top=.9, left=0.15)
+	axes_style = {'linewidth':2}
+	plt.imshow(i3[0,:,:], aspect = 'auto', extent=(np.min(wvl_new),np.max(wvl_new), 1570, 0))
+	plt.title('Spectral Profile Map for Mg II k & h', fontsize = 15)
+	plt.xlabel('Wavelength - 2796.2 [$\AA$]', fontsize = 15)
+	axes_style = {'linewidth':2}
+	plt.ylabel('Time (s)', fontsize = 15)
+	ax = plt.gca()
+	ax.invert_yaxis()
+	plt.savefig('spectral_map.eps')
+	plt.show()
+	plt.show()
