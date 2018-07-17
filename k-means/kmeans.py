16af6a9691f66cdb6189d1183463c4460dabe595
Jamie Huang <jamie.huang@gmail.com>
2018-07-17 10:56:43 -0700

    Update kmeans.py

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -3,7 +3,7 @@ This allows to handle something
 JMS Explain things here.
 '''
 import pickle
-import time
+# import time
 import numpy as np
 import matplotlib.pyplot as plt
 # from sklearn.cluster import KMeans
@@ -37,148 +37,135 @@ class kmeans():
         self.data = np.load(npzfilename)
         self.i_3 = self.data["arr_0"]
         self.wvl = self.data["arr_1"]
-        print("True") #JMS remove this print statement.
-        '''
         if self.verbose:
-            print(some meaningfull message), message, warning.
-        '''
+            print("warning")
 
 
-    def read_data_ncdf(self, rbfilename='output_ray_l2d90x40r.ncdf'): ## ncdfilename
+    def read_data_ncdf(self, ncdffilename='output_ray_l2d90x40r.ncdf'):
         '''
         Reading from the original RH code ncdf files
         '''
 
-        self.wvl1 = nd.getvar(rbfilename, 'wavelength') ## wvl1 -> wvl_orig
-        self.inte = nd.getvar(rbfilename, 'intensity', memmap=True) ## int_orig
+        self.wvl_orig = nd.getvar(ncdffilename, 'wavelength')
+        self.int_orig = nd.getvar(ncdffilename, 'intensity', memmap=True)
 
 
-    def read_data_pck(self, kmeansfilename='k-means.pck'):
-        '''
-        def save_all_pkl(self,filename=''):
+    def read_data_pck(self, filename='k-means.pck'):
+
+        def save_all_pkl(self, filename='k-means.pck'):
             data = self.__dict__
             filehandler = open(filename+'.opy', 'wb')
-            pickle.dump(data, filehandler,protocol=4)
-        def load_all_pkl(self,filename):
+            pickle.dump(data, filehandler, protocol=4)
+        def load_all_pkl(self, filename='k-means.pck'):
             filehandler = open(filename, 'rb')
-            data=pickle.load(filehandler)
+            data = pickle.load(filehandler)
             for idata in data.keys():
-                setattr(self,idata,data[idata])
-        '''
-        pick_in = open(kmeansfilename, 'rb')
-        self.k_m = pickle.load(pick_in)
+                setattr(self, idata, data[idata])
+
+        # pick_in = open(kmeansfilename, 'rb')
+        # self.k_m = pickle.load(pick_in)
 
 
-    def wavelength_distinction(self, delta=5): # spect_lines_limits
+    def spect_lines_limits(self, wvl_delta=5):
         '''
         This allows to cut the wavelength range in different spectral lines.
-        Find where the value of delwvl is larger than the previous index
-        by delta, appends that value into limits array
-        Loops through the limits array, and sees if the value for one index
-        in limits has a greater difference than 1 than the previous index.
+        Find where the value of delwvl_up is larger than the previous index
+        by wvl_delta, appends that value into wvl_lmts array
+        Loops through the wvl_lmts array, and sees if the value for one index
+        in wvl_lmts has a greater difference than 1 than the previous index.
         JMS follow standards of documentation.
         http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
         '''
 
-        delwvl = self.wvl1-shift(self.wvl1, 1) ### delwvl_up
-        new_delwvl = shift(self.wvl1, -1) - self.wvl1  ### delwvl_dn
-        self.limits = [v for v in range(1, len(delwvl)) if np.abs(delwvl[v]-new_delwvl[v]) > delta] ### wvl_lmts
-        self.limits.append(len(self.wvl1))
+        delwvl_up = self.wvl_orig-shift(self.wvl_orig, 1)
+        delwvl_dn = shift(self.wvl_orig, -1) - self.wvl_orig
+        self.wvl_lmts = [v for v in range(1, len(delwvl_up)) 
+                         if np.abs(delwvl_up[v]-delwvl_dn[v]) > wvl_delta]
+        self.wvl_lmts.append(len(self.wvl_orig))
 
-    def print_list_wvl(self, delta=5): # print_line_list
+    def print_line_list(self, wvl_delta=5):
         '''
-        The profile at inte is added to the array new_inte
+        The profile at inte is added to the array int_indv
         '''
 
-        self.new_inte = {}  #Remove this line.
-
-        if not hasattr(self, 'limits'):
-            self.wavelength_distinction(delta)
+        if not hasattr(self, 'wvl_lmts'):
+            self.spect_lines_limits(wvl_delta)
 
         count = 0
-        for ind in range(0, np.size(self.limits)-1):
-            if self.limits[ind+1]-self.limits[ind] > 1:
-                print('%i,wvl=%0.1f' %(count, self.wvl1[self.limits[ind]]))
+        for ind in range(0, np.size(self.wvl_lmts)-1):
+            if self.wvl_lmts[ind+1]-self.wvl_lmts[ind] > 1:
+                print('%i,wvl=%0.1f' %(count, self.wvl_orig[self.wvl_lmts[ind]]))
                 count = count + 1
 
 
-    def individual_spectral_data(self, delta=5): # break_lines, delta->wvl_delta
+    def individual_spectral_data(self, wvl_delta=5): # break_lines
         '''
-        The profile at inte is added to the array new_inte
+        The profile at inte is added to the array int_indv
         '''
-        if not hasattr(self, 'new_inte'):
-            self.new_inte = {} ### int_indv
-        if not hasattr(self, 'new_wvl'):
-            self.new_wvl = {} ### wvl_indv
+        if not hasattr(self, 'int_indv'): 
+            self.int_indv = {}
+        if not hasattr(self, 'wvl_indv'):
+            self.wvl_indv = {}
             print('True')
-
-        self.new_inte = {} #remove
-        self.new_wvl = {} #remove
-
-        if not hasattr(self, 'limits'):
-            self.wavelength_distinction(delta)
+        if not hasattr(self, 'wvl_lmts'):
+            self.spect_lines_limits(wvl_delta)
 
         count = 0
-        for ind in range(0, np.size(self.limits)-1):
-            if self.limits[ind+1]-self.limits[ind] > 1:
-                self.new_inte[count] = self.inte[:, :, self.limits[ind]:self.limits[ind+1]-1] # var
-                self.new_wvl[count] = self.wvl1[self.limits[ind]:self.limits[ind+1]-1]
+        for ind in range(0, np.size(self.wvl_lmts)-1):
+            if self.wvl_lmts[ind+1]-self.wvl_lmts[ind] > 1:
+                self.int_indv[count] = self.int_orig[:, :, self.wvl_lmts[ind]:self.wvl_lmts[ind+1]-1] # var
+                self.wvl_indv[count] = self.wvl_orig[self.wvl_lmts[ind]:self.wvl_lmts[ind+1]-1]
                 count = count + 1
 
-    def km_interp(self, ind=0, savefile=None, nwvl_min=500): #linear_spect, ind->id_lines,
+    def linear_spect(self, id_lines=0, savefile=None, nwvl_min=500):
         '''
         interpolation of the axis
-        plots wvl against new_inte in an uniform axis(wvlax)
+        plots wvl against int_indv in an uniform axis(wvl_linear)
         '''
 
-        self.new_inte1 = {}
+        self.int_linear = {}
 
-        if not hasattr(self, 'wvlax'): # wvl_linear
-            self.wvlax = {}
+        if not hasattr(self, 'wvl_linear'):
+            self.wvl_linear = {}
 
-        delwvl = np.gradient(self.new_wvl[ind])
+        delwvl_up = np.gradient(self.wvl_indv[id_lines])
 
-        if not hasattr(self, 'new_inte1'): # int_linear
-            self.new_inte1 = {}
+        if not hasattr(self, 'int_linear'):
+            self.int_linear = {}
 
-        mindelwvl = np.min(delwvl)
-        n_points = np.min([(np.max(self.new_wvl[ind])-np.min(self.new_wvl[ind]))/mindelwvl, nwvl_min])
-
-
-        inte1 = {} # remove
+        mindelwvl = np.min(delwvl_up)
+        n_points = np.min([(np.max(self.wvl_indv[id_lines])-np.min(self.wvl_indv[id_lines]))/mindelwvl, nwvl_min])
 
         if n_points > 5:
-            max_value = np.max(self.new_wvl[ind])
-            min_value = np.min(self.new_wvl[ind])
-            self.wvlax[ind] = np.linspace(min_value, max_value, num=n_points)
-            inte1 = np.zeros((self.inte.shape[0],
-                              self.inte.shape[1], np.shape(self.wvlax[ind])[0]))
+            max_value = np.max(self.wvl_indv[id_lines])
+            min_value = np.min(self.wvl_indv[id_lines])
+            self.wvl_linear[id_lines] = np.linspace(min_value, max_value, num=n_points)
+            inte1 = np.zeros((self.int_orig.shape[0],
+                              self.int_orig.shape[1], np.shape(self.wvl_linear[id_lines])[0]))
 
-            for ind2 in range(0, len(self.new_inte[ind][:, 0, 0])):
-                for ind3 in range(0, len(self.new_inte[ind][0, :, 0])):
-                    inte1[ind2, ind3, :] = np.interp(self.wvlax[ind],
-                                            self.new_wvl[ind],
-                                            self.new_inte[ind][ind2, ind3, :])
+            for ind2 in range(0, len(self.int_indv[id_lines][:, 0, 0])):
+                for ind3 in range(0, len(self.int_indv[id_lines][0, :, 0])):
+                    inte1[ind2, ind3, :] = np.interp(self.wvl_linear[id_lines],
+                                                     self.wvl_indv[id_lines],
+                                                     self.int_indv[id_lines][ind2, ind3, :])
 
-        self.new_inte1[ind] = inte1
-        '''
-        use verbose for print messages, make a more meaningfull messeage, i.e.,
-        no new_inte1  but something else.
-        '''
-        print('new_inte1', ind, np.shape(self.new_inte1[ind]))
+        self.int_linear[id_lines] = inte1
+
+        if self.verbose:
+            print('int_linear', id_lines, np.shape(self.int_linear[id_lines]))
         if savefile != None:
-            mg2_cube = self.new_inte1[ind]
+            mg2_cube = self.int_linear[id_lines]
             np.save(savefile + '.npy', mg2_cube)
 
 
-    def read_lim_interp(self, rbfilename='/net/opal/Volumes/Amnesia/mpi3drun/2Druns/genohm/rain/output_ray_l2d90x40r.ncdf', ind=0, delta=5):
-        self.read_data_ncdf(rbfilename=rbfilename)
-        self.individual_spectral_data(delta=delta)
-        self.km_interp(ind=ind)
+    def read_lim_interp(self, ncdffilename='/net/opal/Volumes/Amnesia/mpi3drun/2Druns/genohm/rain/output_ray_l2d90x40r.ncdf', ind=0, wvl_delta=5):
+        self.read_data_ncdf(ncdffilename=ncdffilename)
+        self.individual_spectral_data(wvl_delta=wvl_delta)
+        self.linear_spect(ind=ind)
 
 
 
-    def time_import(self, maxnum=5): # km_clusters_???? max_cluster_niter
+    def km_clusters(self, max_cluster_niter=5):
         '''
         uses the MiniBatchKMeans function to fit the i3_2d data into clusters,
         computes the inertia of the MiniBatchKMeans
@@ -186,23 +173,16 @@ class kmeans():
         '''
 
         i3_2d = self.i_3.reshape(self.i_3.shape[0]*self.i_3.shape[1], self.i_3.shape[2]) ## int_linear[line_id]
-        self.t_m = np.zeros(maxnum) # remove
-        self.inertia = np.zeros(maxnum) #km_inertia
-        self.t_zero = time.time() #remove
-        self.t_mini_batch = time.time() - self.t_zero #  remove
-        for i in range(0, maxnum):
+        self.inertia = np.zeros(max_cluster_niter) #km_inertia
+        for i in range(0, max_cluster_niter):
             self.mini_km = MiniBatchKMeans(n_clusters=(i+1)*10, n_init=10).fit(i3_2d)
-            self.t_m[i] = self.t_mini_batch/((i+1)*10) # remove
             self.inertia[i] = self.mini_km.inertia_
             ### save in an array (i+1)*10
-            ### use verbose if statement.
-            print('description iteration=',i)
-        '''
-        plt.plot(the array that contains the infor of n_clusters,self.inertia)
+            if self.verbose:
+                print('description iteration=', i)
+        plt.plot(self.t_m, self.inertia)
         plt.show()
-        '''
         print(self.t_m)
-        return self.mini_km #Remove
 
     def mini_batch_fit(self, ind=0):
         '''
@@ -210,32 +190,30 @@ class kmeans():
         outputs: prints time and inertia
         '''
 
-        self.t_zero = time.time() #remove
+        int_tmp = np.reshape(self.int_linear[0], (self.int_linear[0].shape[0]*self.int_linear[0].shape[1], self.int_linear[0].shape[2]))
+        self.mini_km = MiniBatchKMeans(n_clusters=30).fit(int_tmp) # km_means_list
+        if self.verbose:
+            print("inertia = ", self.inertia)
 
-        self.a = np.reshape(self.new_inte1[0], (self.new_inte1[0].shape[0]*self.new_inte1[0].shape[1], self.new_inte1[0].shape[2])) # a -> int_tmp (no self)
-        self.mini_km = MiniBatchKMeans(n_clusters=30).fit(self.a) # km_means_list
-        print("time = ", self.t_mini_batch) #remove
-        print("inertia = ", self.inertia) #use verbose
-
-    def create_km_map(self,show=True):
+    def create_km_map(self, show=True):
         '''
         creates the image of the km_map_datacube
         shows the locations of the k-means clusters for each labels
         outputs: prints the image of the km_map datacube
         '''
 
-        dim_i_3 = np.shape(self.i_3) # int_linear_shape
+        int_linear_shape = np.shape(self.i_3)
 
-        km_map_datacube = np.zeros((dim_i_3[0], dim_i_3[1], 30))
-        # km_map_datacube = np.zeros((dim_i_3[0], dim_i_3[1]))
+        km_map_datacube = np.zeros((int_linear_shape[0], int_linear_shape[1], 30))
+        # km_map_datacube = np.zeros((int_linear_shape[0], int_linear_shape[1]))
 
         for i in range(0, 30): # 30 must be n_cluster from mini_batch_fit
             plt.subplot(5, 6, i+1)
             plt.xlabel('X [DNs]', fontsize=8)
             plt.ylabel('Time [Seconds]', fontsize=8)
             www = np.where(self.k_m.labels_ == i)[0]
-            xxx = www/dim_i_3[1]
-            yyy = www%dim_i_3[1]
+            xxx = www/int_linear_shape[1]
+            yyy = www%int_linear_shape[1]
             km_map_datacube[xxx.astype(int), yyy.astype(int), i] = i
             #km_map_datacube[xxx.astype(int), yyy.astype(int)] = i
         '''
